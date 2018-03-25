b1589f66f78a2a30050949ce36e941b0ae9e8737
MateuszKita <mkita.zg@gmail.com>
2018-03-25 22:47:51 +0200

    groups are ready to get schedules from cheerio

--- scraping/mainScraper.js
+++ scraping/mainScraper.js
@@ -1,19 +1,19 @@
-const request = require("request");
-const cheerio = require("cheerio");
-const async = require("async");
+const request = require('request');
+const cheerio = require('cheerio');
+const async = require('async');
 
-const uzUrlPrefix = "http://www.plan.uz.zgora.pl/";
+const uzUrlPrefix = 'http://www.plan.uz.zgora.pl/';
 
 const mainScraper = mainUrl => {
   async.waterfall([
     cb => {
       let studyCourses = [];
-      console.log("[1st request] Calling MainUrl");
+      console.log('[1st request] Calling MainUrl');
       request(mainUrl, (er, res) => {
-        if (!er && res.statusCode == "200") {
+        if (!er && res.statusCode == '200') {
           const $ = cheerio.load(res.body);
 
-          $(".list-group-item li a").each((index, el) => {
+          $('.list-group-item li a').each((index, el) => {
             studyCourses.push({
               id: index,
               url: uzUrlPrefix + el.attribs.href,
@@ -22,25 +22,25 @@ const mainScraper = mainUrl => {
             });
           });
         } else {
-          console.log("[1st request ERROR:]", res.statusCode, er);
+          console.log('[1st request ERROR:]', res.statusCode, er);
         }
 
-        console.log("[1st request] OK");
+        console.log('[1st request] OK');
         cb(null, studyCourses);
       });
     },
     (studyCourses, cb) => {
-      console.log("[2nd request] Calling studyCourses URLs");
+      console.log('[2nd request] Calling studyCourses URLs');
       let studyCoursesWithGroups = [];
 
       studyCourses.map(studyCourse => {
         request(studyCourse.url, (er, res) => {
           if (er) {
-            console.log(studyCourse.id + " studyCourses ERROR:", er);
+            console.log(studyCourse.id + ' studyCourses ERROR:', er);
           } else {
             const $ = cheerio.load(res.body);
 
-            $("tbody tr td a").each((index, el) => {
+            $('tbody tr td a').each((index, el) => {
               studyCourse.groups.push({
                 id: index,
                 name: el.children[0].data,
@@ -52,26 +52,38 @@ const mainScraper = mainUrl => {
           studyCoursesWithGroups.push(studyCourse);
 
           if (studyCoursesWithGroups.length == studyCourses.length) {
-            console.log("[2nd request] OK");
+            console.log('[2nd request] OK');
             cb(null, studyCoursesWithGroups);
           }
         });
       });
     },
     (studyCoursesWithGroups, cb) => {
-      console.log("[3rd request] Calling study courses groups' URLs");
-      let studyCoursesWithGroupsAmount = 0;
-      let newStudyCoursesWithGroupsAmount = 0;
-      studyCoursesWithGroups.forEach(studyCoursesWithGroup => {
-        studyCoursesWithGroupsAmount += studyCoursesWithGroup.groups.length;
-      });
+      console.log(
+        "[3rd request] Calling study courses groups' URLs with schedules"
+      );
+      studyCoursesWithGroups = studyCoursesWithGroups.sort(
+        (course, another) => {
+          if (course.id < another.id) return -1;
+          if (course.id > another.id) return 1;
+          return 0;
+        }
+      );
+      let groupsUrlsAmount = 0;
+      let processCounter = 0;
 
-      let newStudyCoursesWithGroups = [];
+      studyCoursesWithGroups.forEach(studyCoursesWithGroup => {
+        groupsUrlsAmount += studyCoursesWithGroup.groups.length;
+      });
+      let time = 0;
       studyCoursesWithGroups.map(studyCourseWithGroups => {
         studyCourseWithGroups.groups.map(group => {
-          request(group.url, (er, res) => {
-            if (res.statusCode) {
-              if (!er && res.statusCode == "200") {
+          time += 100;
+          setTimeout(() => {
+            request(group.url, (er, res) => {
+              group.schedule = [];
+              processCounter++;
+              if (!er) {
                 // const $ = cheerio.load(res.body);
                 // $(".list-group-item li a").each((index, el) => {
                 //   studyCourses.push({
@@ -81,24 +93,26 @@ const mainScraper = mainUrl => {
                 //     groups: []
                 //   });
                 // });
-                newStudyCoursesWithGroups.push(studyCourseWithGroups);
+                if (processCounter == groupsUrlsAmount) {
+                  console.log('[3rd request] OK');
+                  cb(null, studyCoursesWithGroups);
+                } else {
+                  console.log(
+                    'Processing: ' +
+                      (processCounter / groupsUrlsAmount * 100).toFixed(2) +
+                      '%'
+                  );
+                }
               } else {
-                console.log("[3rd request] ERROR: ", res.statusCode, er);
+                console.log('[3rd request] ERROR: ' + er);
               }
-              newStudyCoursesWithGroupsAmount++;
-              if (
-                studyCoursesWithGroupsAmount == newStudyCoursesWithGroupsAmount
-              ) {
-                console.log("[3rd request] OK");
-                cb(null, newStudyCoursesWithGroups);
-              }
-            }
-          });
+            });
+          }, 3000 + time);
         });
       });
     },
     (studyCoursesWithGroupsPlans, cb) => {
-      console.log("[4rd request]");
+      console.log('[4rd request]');
       studyCoursesWithGroupsPlans = studyCoursesWithGroupsPlans.sort(
         (course, another) => {
           if (course.id < another.id) return -1;
